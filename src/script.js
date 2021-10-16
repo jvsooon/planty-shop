fd207597a6aa5728d341e4bc5e140d7b070892f7
Jason Sabal <jason.sabal.861@my.csun.edu>
2021-10-16 01:58:40 -0700

    Add and style header, hero, new arrivals and categories section for mobile first layout

---
+++ src/script.js
@@ -0,0 +1,16 @@
+const menuBtn = document.querySelector('.menu-btn'),
+menuCloseBtn = document.querySelector('.menu-btn-close'),
+navMenu = document.querySelector('.nav__menu');
+
+menuBtn.addEventListener('click', () => {
+    console.log('hi')
+    navMenu.classList.add('menu-show');
+})
+
+menuCloseBtn.addEventListener('click', () => {
+    navMenu.classList.remove('menu-show');
+})
+
+$('.carousel').flickity({
+    cellAlign: 'center'
+});
