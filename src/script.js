1e14adfd08002d294fdff4b3542265de3eb9a669
Jason Sabal <jason.sabal.861@my.csun.edu>
2021-11-04 23:08:39 -0700

    Update navbar drawer open/close styling

--- src/script.js
+++ src/script.js
@@ -1,15 +1,21 @@
 const menuBtn = document.querySelector('.menu-btn'),
 menuCloseBtn = document.querySelector('.menu-btn-close'),
-navMenu = document.querySelector('.nav__menu');
+navMenu = document.querySelector('.nav__menu'),
+navLinks = document.querySelectorAll('.nav__menu li');
 
 menuBtn.addEventListener('click', () => {
-    console.log('hi')
-    navMenu.classList.add('menu-show');
+    navMenu.classList.add('active');
 })
 
 menuCloseBtn.addEventListener('click', () => {
-    navMenu.classList.remove('menu-show');
+    navMenu.classList.remove('active');
 })
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+    });
+});
 
 $('.carousel').flickity({
     initialIndex: 1
